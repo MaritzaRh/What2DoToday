145776022df37019a500360f04fa4a69bae6063d
unknown <maritza_rh97@hotmail.com>
2019-09-22 00:22:11 -0500

    Finish splashscreen

--- app/src/main/java/com/testing/what2dotoday/SplashScreenActivity.kt
+++ app/src/main/java/com/testing/what2dotoday/SplashScreenActivity.kt
@@ -3,26 +3,27 @@ package com.testing.what2dotoday
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import java.lang.Exception
 import kotlin.system.measureTimeMillis
 
 class SplashScreenActivity : AppCompatActivity() {
+    private val splashTime = 2000L
+    private lateinit var myhandler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
-        val background = object: Thread(){
-            override fun run(){
-                try{
-                    sleep(measureTimeMillis { 5000 })
-                    val intent = Intent(baseContext, MainActivity::class.java)
-                    startActivity(intent)
-                } catch (e: Exception){
-                    e.printStackTrace()
-                }
-            }
-        }
-        background.start()
+        myhandler = Handler()
+        myhandler.postDelayed({
+            goToMainActivity()
+        },splashTime)
+    }
+
+    private fun goToMainActivity() {
+        val mainActivityIntent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(mainActivityIntent)
+        finish()
     }
 }
